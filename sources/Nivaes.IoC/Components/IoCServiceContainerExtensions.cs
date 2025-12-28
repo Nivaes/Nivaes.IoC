1bd58ff93c8ef400790641d1fe98367d4225c0a8
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2025-12-28 17:36:15 +0100

    Marck TService with Dynamically Accessed.

--- sources/Nivaes.IoC/Components/IoCServiceContainerExtensions.cs
+++ sources/Nivaes.IoC/Components/IoCServiceContainerExtensions.cs
@@ -1,25 +1,33 @@
-﻿namespace Nivaes.IoC
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nivaes.IoC
 {
     public static class IoCServiceContainerExtensions
     {
-        public static TService? Resolve<TService>(this IIoCResolver container)
+        extension(IIoCResolver container)
         {
-            return (TService?)container.Resolve(typeof(TService));
-        }
-        
-        public static TService? Resolve<TService>(this IIoCResolver container, IOverrides overrides)
-        {
-            return (TService?)container.Resolve(typeof(TService), overrides);
+            public TService? Resolve<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>()
+            {
+                return (TService?)container.Resolve(typeof(TService));
+            }
+
+            public TService? Resolve<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(IOverrides overrides)
+            {
+                return (TService?)container.Resolve(typeof(TService), overrides);
+            }
         }
 
-        public static void AddDelegate<TService>(this IoCServiceContainer container, Func<IIoCResolver, TService?> resolver, Reuse reuse = Reuse.Transient)
+        extension(IoCServiceContainer container)
         {
-            container.AddDelegate(r => resolver(r)!, typeof(TService), reuse);
-        }
-        
-        public static void ReplaceDelegate<TService>(this IoCServiceContainer container, Func<IIoCResolver, TService?> resolver, Reuse reuse = Reuse.Transient)
-        {
-            container.ReplaceDelegate(r => resolver(r)!, typeof(TService), reuse);
+            public void AddDelegate<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(Func<IIoCResolver, TService?> resolver, Reuse reuse = Reuse.Transient)
+            {
+                container.AddDelegate(r => resolver(r)!, typeof(TService), reuse);
+            }
+
+            public void ReplaceDelegate<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(Func<IIoCResolver, TService?> resolver, Reuse reuse = Reuse.Transient)
+            {
+                container.ReplaceDelegate(r => resolver(r)!, typeof(TService), reuse);
+            }
         }
     }
 }
